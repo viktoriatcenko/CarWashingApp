ff8d899d07b479d91964aec0ce44963f642fee50
Sergey <116721624+monocvet@users.noreply.github.com>
2024-07-23 14:29:30 +0300

    поправки

--- src/main/java/ru/car/washing/controller/OfferController.java
+++ src/main/java/ru/car/washing/controller/OfferController.java
@@ -1,11 +1,9 @@
 package ru.car.washing.controller;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 import ru.car.washing.model.Offer;
 import ru.car.washing.services.OfferService;
 
